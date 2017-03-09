92d1e0412940573c65a843cceeedc71da1a67f07
Matthew Stafford <matthew.stafford@bluelabs.com>
2017-03-09 11:04:45 -0500

    select right form

--- DSA_donation_button.py
+++ DSA_donation_button.py
@@ -16,7 +16,7 @@ br = mechanize.Browser(factory=mechanize.RobustFactory())
 def lambda_handler(event, context):
 	br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 	br.open("https://dsausa.nationbuilder.com/donate")
-	br.select_form(nr=3)
+	br.select_form(nr=2)
 
 	br.form['submitted[donation][aclu_recurring]']=0 
 	br.form['donation[amount_option]']=["other"]
